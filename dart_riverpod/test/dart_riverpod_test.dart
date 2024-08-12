6ac84d82bce74f0e7028e14539ebefe8b73a1494
ChloeAngela <142189962+ChloeAngela@users.noreply.github.com>
2024-08-12 15:39:58 +0800

    Add files via upload

---
+++ dart_riverpod/test/dart_riverpod_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_riverpod/dart_riverpod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
